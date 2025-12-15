d14b40b4cdd26391f3bcbfa9ce898303c60ec2c7
Elias Ferreira Junior <elias.junior@etipi.pi.gov.br>
2025-12-15 10:21:54 -0300

    feat: Integrate Action Text and Active Storage for rich text post content.

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "lexxy", "~> 0.1.23.beta"
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  ## VALIDATIONS
   validates :title, presence: true
   validates :content, presence: true, length: { minimum: 10 }
+
+  ## ASSOCIATIONS
+  has_rich_text :content
 end
