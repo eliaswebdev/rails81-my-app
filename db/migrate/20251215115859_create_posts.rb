e77eb4beda1d3a6cc3e706ffe93a03ec0f88ef83
Elias Ferreira Junior <elias.junior@etipi.pi.gov.br>
2025-12-15 09:03:17 -0300

    initial commit

---
+++ db/migrate/20251215115859_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[8.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.integer :status
+      t.boolean :active
+      t.datetime :published_at
+
+      t.timestamps
+    end
+  end
+end
